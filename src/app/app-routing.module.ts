8ae30099e243c8f60751a3839a761e199ced67eb
keita <kmahamadou858@gmail.com>
2022-09-09 01:18:10 +0200

    definition des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { DetailsListeComponent } from './details-liste/details-liste.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'',
+    redirectTo:"/accueil",
+    pathMatch:"full"
+  },
+  { path:'accueil',component:AccueilComponent }, 
+  { path:'detailsl',component:DetailsListeComponent }, 
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
