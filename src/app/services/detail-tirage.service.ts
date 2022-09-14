4b84b13e9f996d168002a9f493b01c1255a178cb
ADIAWIAKOYE <adiawiakoye.le10@gmail.com>
2022-09-14 12:40:26 +0000

    nouvelle mise a jour

--- src/app/services/detail-tirage.service.ts
+++ src/app/services/detail-tirage.service.ts
@@ -8,21 +8,16 @@ import { Observable } from 'rxjs';
 export class DetailTirageService {
 
   constructor(private http:HttpClient) { }
-
-<<<<<<< HEAD
-    return this.http.get("http://localhost:8080/PostulantTrie/recupererPostulant/Tirage1_liste");
-=======
+     
+    // return this.http.get("http://localhost:8080/PostulantTrie/recupererPostulant/Tirage1_liste");
 
   getListePostulanttirageByNomTirage(tirageid: number) : Observable<object>{
 
     return this.http.get<object>(`http://localhost:8080/PostulantTrie/recupererPostulantid/${tirageid}`);
->>>>>>> 69ab7aa5f7e170ac4b02c117c0a73e3ffbd77972
 
   }
 
   getAllTirages() : Observable<object>{
     return this.http.get<object>("http://localhost:8080/tirage/recupererTirages");
   }
-
-
 }
